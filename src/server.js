cb8c92c4be279e0fd9165333b75088b4fca57a8d
rockmuthu <rockmuthu345@gmail.com>
2026-03-19 21:29:05 +0530

    server js channge

--- src/server.js
+++ src/server.js
@@ -1,12 +1,18 @@
 
 import dotenv from "dotenv";
-dotenv.config()
+dotenv.config();
 
 import app from "./app.js";
 
 const PORT = process.env.PORT || 8080;
 
-console.log("DATABASE_URL:", process.env.DATABASE_URL?"FOUND" : "MISSING");
-app.listen(PORT,"0.0.0.0",() => {
+// ⭐ HEALTH ROUTE (must be here)
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
+
+console.log("DATABASE_URL:", process.env.DATABASE_URL ? "FOUND" : "MISSING");
+
+app.listen(PORT, "0.0.0.0", () => {
   console.log("🚀 NexBills Backend Running on", PORT);
 });
