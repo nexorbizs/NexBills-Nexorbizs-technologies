3c3226e45b6d853fbbbde06405e6fabf0fa22aa0
rockmuthu <rockmuthu345@gmail.com>
2026-03-19 22:11:38 +0530

    exception added

--- src/server.js
+++ src/server.js
@@ -10,3 +10,11 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, "0.0.0.0", () => {
   console.log("🚀 NexBills Backend Running on", PORT);
 });
+
+process.on("uncaughtException", (err) => {
+  console.error("Uncaught Exception:", err);
+});
+
+process.on("unhandledRejection", (reason) => {
+  console.error("Unhandled Rejection:", reason);
+});
