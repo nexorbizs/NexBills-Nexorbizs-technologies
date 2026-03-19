9fbf63e07228ec16541fb9f211caf6808784c3f8
rockmuthu <rockmuthu345@gmail.com>
2026-03-19 20:28:22 +0530

    port final fix

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import app from "./app.js";
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
+app.listen(PORT,"0.0.0.0",() => {
   console.log("🚀 NexBills Backend Running on", PORT);
 });
