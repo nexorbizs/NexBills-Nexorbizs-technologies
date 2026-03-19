ec0fdf7a9325edf2be28c75afad8c746913bb523
rockmuthu <rockmuthu345@gmail.com>
2026-03-19 20:41:44 +0530

    env file final

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
+
+import dotenv from "dotenv";
+dotenv.config()
+
 import app from "./app.js";
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
+console.log("DATABASE_URL:", process.env.DATABASE_URL?"FOUND" : "MISSING");
 app.listen(PORT,"0.0.0.0",() => {
   console.log("🚀 NexBills Backend Running on", PORT);
 });
